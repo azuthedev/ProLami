a31b566d2e039d3ef76cf962e7d5adcd1775d908
Azu XIII AI <officialycrec@gmail.com>
2026-03-18 22:48:50 +0100

    Update CTA link in FAQ component

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -37,7 +37,7 @@ const CONTENT = {
   ],
   cta: {
     text: 'Boka tid',
-    href: 'https://www.bokadirekt.se/places/odenplanskliniken-35930',
+    href: 'https://www.bokadirekt.se/places/prolamination-38424',
   },
 };
 
